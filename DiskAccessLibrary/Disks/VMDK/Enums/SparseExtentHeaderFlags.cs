6f72401bf8eb8c39e39d8fd2cbd959139461ccd8
Tal Aloni <tal.aloni.il@gmail.com>
2025-04-16 01:51:46 +0300

    SparseExtentHeaderFlags: Added UseZeroedGrainGTEs flag

--- DiskAccessLibrary/Disks/VMDK/Enums/SparseExtentHeaderFlags.cs
+++ DiskAccessLibrary/Disks/VMDK/Enums/SparseExtentHeaderFlags.cs
@@ -7,6 +7,7 @@ namespace DiskAccessLibrary.VMDK
     {
         ValidNewLineDetectionTest = 0x00000001,
         HasRedundantGrainTable = 0x00000002,
+        UseZeroedGrainGTEs = 0x00000004, // SparseExtentHeader version 2 and above, see Virtual Disk Format 5.0
         UseCompressionForGrains = 0x00010000,
         HasMarkers = 0x00020000,
     }
