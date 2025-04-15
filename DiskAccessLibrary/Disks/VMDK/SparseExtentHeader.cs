478972989d9b0544f54d29a2293093c5f133f920
Tal Aloni <tal.aloni.il@gmail.com>
2025-04-16 01:52:47 +0300

    SparseExtentHeader: Add UseCompressionForGrains property

--- DiskAccessLibrary/Disks/VMDK/SparseExtentHeader.cs
+++ DiskAccessLibrary/Disks/VMDK/SparseExtentHeader.cs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014-2023 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+/* Copyright (C) 2014-2025 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
@@ -121,6 +121,14 @@ namespace DiskAccessLibrary.VMDK
             get
             {
                 return (Flags & SparseExtentHeaderFlags.HasRedundantGrainTable) > 0;
+            }
+        }
+
+        public bool UseCompressionForGrains
+        {
+            get
+            {
+                return (Flags & SparseExtentHeaderFlags.UseCompressionForGrains) > 0;
             }
         }
     }
